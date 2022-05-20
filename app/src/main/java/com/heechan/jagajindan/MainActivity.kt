9a2014d9e1bdfd976366e4a3593397378c13620b
Heechan <ckstmznf@naver.com>
2022-05-20 11:22:19 +0900

    이름 입력

--- app/src/main/java/com/heechan/jagajindan/MainActivity.kt
+++ app/src/main/java/com/heechan/jagajindan/MainActivity.kt
@@ -9,16 +9,16 @@ import com.heechan.jagajindan.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding;
 
-    var BASE_URL = "https://jagajindan.netlify.app/?username=박희찬"
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
+        val URL = "https://jagajindan.netlify.app/?username=${intent.getStringExtra("name")}"
+
         binding.wvMain.apply {
             settings.javaScriptEnabled = true
-            loadUrl(BASE_URL)
-//            webChromeClient = WebChromeClient()
+            loadUrl(URL)
+            webChromeClient = WebChromeClient()
         }
 
     }
